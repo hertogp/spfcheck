0a424605e536ba2d68896e6abffcc8d59c6b4852
hertogp <git.hertogp@gmail.com>
2021-08-28 14:43:57 +0200

    spfcheck options

--- lib/spfcheck.ex
+++ lib/spfcheck.ex
@@ -12,16 +12,31 @@ defmodule Spfcheck do
     dns: :string,
     input: :string,
     csv: :boolean,
-    help: :boolean
+    help: :boolean,
+    local: :string
+  ]
+
+  @aliases [
+    i: :ip,
+    s: :sender,
+    v: :verbosity,
+    h: :help,
+    l: :local
   ]
 
   @doc """
   Check spf for given ip, sender and domain.
   """
   def main(argv) do
-    {parsed, args, invalid} = OptionParser.parse(argv, strict: @options)
+    {parsed, [domain], invalid} = OptionParser.parse(argv, aliases: @aliases, strict: @options)
     IO.inspect(parsed, label: :parsed)
-    IO.inspect(args, label: :args)
+    IO.inspect(domain, label: :domain)
     IO.inspect(invalid, label: :invalid)
+
+    {verdict, explain, term} = Spf.check(domain, parsed)
+
+    exp = if explain != "", do: " (#{explain})", else: ""
+    term = if term, do: ", match by #{inspect(term)}", else: ", nothing matched"
+    IO.puts("#{verdict}#{exp}#{term}")
   end
 end
